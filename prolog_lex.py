ecce91e39062f055bb618d6b36f6fc2b74e6a631
Petya Surkov <petyo333@gmail.com>
2020-10-03 13:30:23 +0300

    Rename bracket tokens

--- prolog_lex.py
+++ prolog_lex.py
@@ -6,8 +6,8 @@ tokens = [
     'DOT',
     'COMMA',
     'SEMICOLON',
-    'LBRACKET',
-    'RBRACKET',
+    'LBR',
+    'RBR',
 ]
 
 
@@ -20,8 +20,8 @@ t_DECLARE = r':-'
 t_DOT = r'\.'
 t_COMMA = r','
 t_SEMICOLON = r';'
-t_LBRACKET = r'\('
-t_RBRACKET = r'\)'
+t_LBR = r'\('
+t_RBR = r'\)'
 
 t_ignore = ' \t'
 
@@ -36,14 +36,5 @@ def t_error(t):
     t.lexer.skip(1)
 
 
-def lex_text(text):
-    lexer = lex.lex()
-    lexer.input(text)
-    lexemes = []
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break
-        lexemes.append(tok)
-    return lexemes
+lexer = lex.lex()
 
